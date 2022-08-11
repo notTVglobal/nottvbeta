af1df391cfb2acddbef9bc2baa17892ef7e381a4
Travis Cross <travis@grandcross.ca>
2022-08-10 23:29:36 -0700

    fix URL for Spring

--- resources/js/Components/VideoPlayer/VideoPlayer.vue
+++ resources/js/Components/VideoPlayer/VideoPlayer.vue
@@ -151,7 +151,7 @@ export default {
                 sources: [
                     {
                         src:
-                            'https://mist.nottv.io:4433/hls/spring/index.m3u8',
+                            'https://mist.nottv.io/hls/spring/index.m3u8',
                             // 'ws://mist.nottv.io:8080/ctd1984.mp4',
                         type: 'application/x-mpegURL'
                     }
