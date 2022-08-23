3a7edc6a29530c79cc03a7a4bf38e4a425d17159
Travis Cross <travis@grandcross.ca>
2022-08-23 00:39:43 -0700

    fix foreign key on roles table

--- database/migrations/2022_08_19_002308_create_roles_table.php
+++ database/migrations/2022_08_19_002308_create_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->timestamps();
             $table->string('role');
         });
