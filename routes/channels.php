45d9e223889b41e736216b3a59fe81c41516be85
Travis Cross <travis@grandcross.ca>
2022-10-14 07:06:36 -0700

    fix production bug with chat updating

--- routes/channels.php
+++ routes/channels.php
@@ -20,7 +20,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 
     Broadcast::channel('chat.{channelId}', function ($user, $channelId) {
         if( Auth::check() ) {
-            return ['id' => $user->id, 'name' => $user->name, 'profilePhoto' => $user->profile_photo_path];
+            return ['id' => $user->id, 'name' => $user->name];
         }
 });
 
