a638be549ecded02ef44704a94bced406fea7bef
Travis Cross <travis@grandcross.ca>
2022-11-23 23:32:33 -0800

    change mistAddress to mist.not.tv

--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -366,8 +366,8 @@ let md5 = require('md5');
 //
 // let mistAddress = 'http://localhost:4242/api'
 // let mistAddress = 'https://beta-staging.not.tv/mistserver/api'
-// let mistAddress = 'https://mist.not.tv/'
-let mistAddress = 'http://10.124.0.3:4242/api'
+let mistAddress = 'https://mist.not.tv/'
+// let mistAddress = 'http://10.124.0.3:4242/api'
 // let mistAddress = 'http://localhost:4242/api'
 // let mistAddress = 'http://mist.nottv.io:4242/api'
 // let mistAddressWs = 'ws://mist.nottv.io:4242/ws'
