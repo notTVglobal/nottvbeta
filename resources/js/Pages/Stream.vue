ac59f2ed974f7ffc9034b0216927c82a3cc92343
Travis Cross <travis@grandcross.ca>
2022-12-04 21:02:22 -0800

    add testing page for CSS

--- resources/js/Pages/Stream.vue
+++ resources/js/Pages/Stream.vue
@@ -16,7 +16,7 @@ import { useVideoPlayerStore } from "@/Stores/VideoPlayerStore.js"
 import { useStreamStore } from "@/Stores/StreamStore.js"
 import { useChatStore } from "@/Stores/ChatStore.js"
 import ChatForStreamPageV2 from "@/Components/Chat/ChatForStreamPageV2"
-import {onBeforeMount} from "vue";
+import { onBeforeMount, onMounted } from "vue";
 
 let videoPlayerStore = useVideoPlayerStore()
 let streamStore = useStreamStore()
@@ -24,14 +24,16 @@ let chatStore = useChatStore()
 
 onBeforeMount(() => {
     videoPlayerStore.makeVideoFullPage();
+    
+});
 
+onMounted(() => {
     if (streamStore.currentChannel != 'Stream') {
         videoPlayerStore.videoSource = 'https://mist2.not.tv/hls/dunepull/index.m3u8';
         videoPlayerStore.videoName = 'Dune';
         videoPlayerStore.loadNewSource()
     }
-
-});
+})
 chatStore.showChat = false
 videoPlayerStore.loggedIn = true
 videoPlayerStore.currentView = 'stream'
