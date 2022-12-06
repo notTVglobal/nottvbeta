4a25f51561cbee3c97a54194551c9f22ca61a311
Travis Cross <travis@grandcross.ca>
2022-12-06 06:31:41 -0800

    increase OTT Buttons height

--- resources/js/Components/VideoPlayer/VideoOTTButtons.vue
+++ resources/js/Components/VideoPlayer/VideoOTTButtons.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row justify-around h-6 bg-red-500 w-full text-xs uppercase font-semibold">
+    <div class="flex flex-row justify-around h-7 bg-red-500 w-full text-xs uppercase font-semibold">
 
         <button class="h-full w-full"
                 :class="{ 'bg-green-900': videoPlayerStore.ott != 1, 'bg-green-700': videoPlayerStore.ott === 1, 'hover:bg-green-700':videoPlayerStore.ott != 1 }"
