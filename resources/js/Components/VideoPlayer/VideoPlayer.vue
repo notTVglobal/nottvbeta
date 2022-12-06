0ff7c80725772979f366595d1527a1d5efe6a2a1
Travis Cross <travis@grandcross.ca>
2022-12-06 04:43:29 -0800

    resizing video player fixed on mobile

--- resources/js/Components/VideoPlayer/VideoPlayer.vue
+++ resources/js/Components/VideoPlayer/VideoPlayer.vue
@@ -70,13 +70,13 @@
                 <button v-if="!streamStore.showOSD && $page.props.user!=null" @click="streamStore.toggleChannels()"
                         class="opacity-80 chatButtonForStreamPage w-20 h-20 bottom-6 mr-24 rounded-full bg-green-400 text-green-100
                                hover:bg-green-600 hover:text-green-300 cursor-pointer grid justify-center content-center text-xs">
-                    <font-awesome-icon icon="fa-rocket" class="ml-3 text-3xl"/><div>CHANNELS</div>
+                    <font-awesome-icon icon="fa-rocket" class="ml-4 text-3xl"/><div>CHANNELS</div>
                 </button>
 
                 <button v-if="!streamStore.showOSD && $page.props.user!=null"  @click="streamStore.toggleChat()"
                         class="opacity-80 chatButtonForStreamPage w-20 h-20 bottom-6 rounded-full bg-orange-400 text-orange-100
                                hover:bg-orange-600 hover:text-orange-300 cursor-pointer grid justify-center content-center">
-                    <font-awesome-icon icon="fa-comments" class="text-3xl"/><div>CHAT1</div>
+                    <font-awesome-icon icon="fa-comments" class="text-3xl"/><div>CHAT</div>
                 </button>
 
                 <div v-if="userStore.isMobile">
