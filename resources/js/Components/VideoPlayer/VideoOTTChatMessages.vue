af9c8fdc899d871546edb7b246ca32334191aea0
Travis Cross <travis@grandcross.ca>
2022-12-06 21:14:22 -0800

    change bottom position of chat on topRight

--- resources/js/Components/VideoPlayer/VideoOTTChatMessages.vue
+++ resources/js/Components/VideoPlayer/VideoOTTChatMessages.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="pb-64">
         <button @click.prevent="scrollTo('#scrollToMe')" class="bottom-0 mr-32 h-12 bg-blue-800 hover:bg-blue-600 w-56 rounded-lg hidden">CLICK HERE TO SCROLL TO BOTTOM</button>
-        <div class="chatChrome w-full h-full pb-36 pt-5 bottom-36 flex flex-col-reverse overflow-y-scroll overflow-x-clip break-words messages">
+        <div class="chatChrome w-full h-full pb-36 pt-5 bottom-48 flex flex-col-reverse overflow-y-scroll overflow-x-clip break-words messages">
             <div id="scrollToMe"></div>
             <div v-for="(message, index) in chatStore.messages.slice()" :key="index">
                 <message-item :id="message.id" :message="message" :time="time(message.created_at)" />
