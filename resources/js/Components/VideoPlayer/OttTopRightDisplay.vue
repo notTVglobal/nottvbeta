bf8d6b332b273d91e5449ab1e6979401f90a3340
Travis Cross <travis@grandcross.ca>
2022-12-11 02:37:46 -0800

    add notnow pilot to the list of channels

--- resources/js/Components/VideoPlayer/OttTopRightDisplay.vue
+++ resources/js/Components/VideoPlayer/OttTopRightDisplay.vue
@@ -9,6 +9,7 @@
             <div class="w-full px-4 py-2 bg-green-700 hover:bg-green-500 text-white cursor-pointer border-b border-0.2 border-green-800" @click="changeChannel('live')">LIVE</div>
             <div class="w-full px-4 py-2 bg-green-700 hover:bg-green-500 text-white cursor-pointer border-b border-0.2 border-green-800" @click="changeChannel('stream')">STREAM</div>
             <div class="w-full px-4 py-2 bg-green-700 hover:bg-green-500 text-white cursor-pointer border-b border-0.2 border-green-800" @click="changeChannel('vmixsource03')">VMIX SOURCE 03</div>
+            <div class="w-full px-4 py-2 bg-green-700 hover:bg-green-500 text-white cursor-pointer border-b border-0.2 border-green-800" @click="changeChannel('notnowpilotpull')">NOT NOW</div>
         </div>
 
         <div v-if="videoPlayerStore.ott === 2" class="now-playing w-full h-full bg-purple-800 p-2">
@@ -87,6 +88,12 @@ function changeChannel(name) {
         let source = 'labyrinth'
         videoPlayerStore.videoName = 'Labyrinth'
         streamStore.currentChannel = 'Stream'
+        videoPlayerStore.loadNewSourceFromMist(source)
+    }
+    if (name==='notnowpilotpull') {
+        let source = 'notnowpilotpull'
+        videoPlayerStore.videoName = 'NotNow.tv Pilot'
+        streamStore.currentChannel = 'NotNow'
         videoPlayerStore.loadNewSourceFromMist(source)
     }
     // if (name==='live') {
