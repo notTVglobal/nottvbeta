8df13d16e0e9328a8875536a2e43675653ecfdff
Travis Cross <travis@not.tv>
2023-03-15 07:28:00 -0600

    temporary disable links on home page and add message 'returning soon'

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,20 +5,20 @@
             <header class="headerContainer w-full">
                 <div class="welcomeOverlay flex flex-row md:px-6 py-4 w-full">
                     <WelcomeBug />
-                    <div class="flex justify-end pt-4 md:pr-6 w-full">
-                            <Button class="bg-opacity-50 hover:bg-opacity-75 text-sm md:text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md" v-if="!$page.props.user" @click="welcomeStore.showLogin = true" >
-                                Log in
-                            </Button>
-                            <Button class="bg-opacity-50 hover:bg-opacity-75 text-sm -mr-4 md:mr-0 ml-2 md:text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md" v-if="!$page.props.user" @click="welcomeStore.showRegister = true" >
-                                <!--           <Button class="bg-opacity-0 hover:bg-opacity-0"><Link v-if="!$page.props.user" :href="route('register')" class="text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md">-->
+<!--                    <div class="flex justify-end pt-4 md:pr-6 w-full">-->
+<!--                            <Button class="bg-opacity-50 hover:bg-opacity-75 text-sm md:text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md" v-if="!$page.props.user" @click="welcomeStore.showLogin = true" >-->
+<!--                                Log in-->
+<!--                            </Button>-->
+<!--                            <Button class="bg-opacity-50 hover:bg-opacity-75 text-sm -mr-4 md:mr-0 ml-2 md:text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md" v-if="!$page.props.user" @click="welcomeStore.showRegister = true" >-->
+<!--                                &lt;!&ndash;           <Button class="bg-opacity-0 hover:bg-opacity-0"><Link v-if="!$page.props.user" :href="route('register')" class="text-2xl text-gray-200 hover:text-blue-600 drop-shadow-md">&ndash;&gt;-->
 
-                                Register
-                            </Button>
-                            <Button class="bg-opacity-0 hover:bg-opacity-0"><Link v-if="$page.props.user" :href="route('stream')" class="text-sm md:text-2xl top-20 text-gray-200 underline">
-                                Return to stream
-                            </Link></Button>
+<!--                                Register-->
+<!--                            </Button>-->
+<!--                            <Button class="bg-opacity-0 hover:bg-opacity-0"><Link v-if="$page.props.user" :href="route('stream')" class="text-sm md:text-2xl top-20 text-gray-200 underline">-->
+<!--                                Return to stream-->
+<!--                            </Link></Button>-->
 
-                    </div>
+<!--                    </div>-->
                 </div>
             </header>
 
@@ -38,7 +38,7 @@
 
                     <div class="text-center font-semibold text-3xl">Welcome to notTV</div>
                     <div class="text-center text-2xl">Where artists own the platform.</div>
-                    <div class="mt-32 text-center italic">(Log in to chat)</div>
+<!--                    <div class="mt-32 text-center italic">(Log in to chat)</div>-->
 
             </section>
             <section class="grid md:grid-cols-2 min-h-screen content-center gap-10 bg-gray-300 text-white p-10">
