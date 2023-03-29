a3b6afedb5ca52ea4ac49f1935e547f595461798
Travis Cross <travis@not.tv>
2023-03-29 15:26:38 -0600

    revert uploader to try and fix

--- resources/js/Components/Uploaders/VideoUpload.vue
+++ resources/js/Components/Uploaders/VideoUpload.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
 
-        <progress v-show="userStore.uploadPercentage !== 0" max="100" :value="userStore.uploadPercentage" class="w-full mb-4" />
+        <progress v-show="userStore.uploadPercentage != 0" max="100" :value="userStore.uploadPercentage" class="w-full mb-4" />
 
-        <div v-show="uploadingMessage !== 0" class="mb-4 font-bold text-center">Please stay on this screen until upload is complete.</div>
-        <div v-show="uploadCompleteMessage !== 0" class="mb-4 font-bold text-center">Upload is complete. The video is now processing.</div>
-<!--        <form v-show="!isHidden" id="videoUploadForm" action="/videoupload" class="dropzone dropzoneFile border border-black rounded w-full h-48 max-w-md px-2 py-2 mb-6">-->
-        <form v-show="uploadingMessage === 0" id="videoUploadForm" action="/videoupload" class="dropzone dropzoneFile border border-black rounded w-full h-48 max-w-md px-2 py-2 mb-6">
+        <div v-show="uploadingMessage" class="mb-4 font-bold text-center">Please stay on this screen until upload is complete.</div>
+        <div v-show="uploadCompleteMessage" class="mb-4 font-bold text-center">Upload is complete. The video is now processing.</div>
+        <form v-show="!isHidden" id="videoUploadForm" action="/videoupload" class="dropzone dropzoneFile border border-black rounded w-full h-48 max-w-md px-2 py-2 mb-6">
             <!--                            add input fields and a submit button to send data back to Laravel -->
-            <input hidden name="movieId" v-model="form.movieId">
-            <input hidden name="movieTrailerId" v-model="form.movieTrailerId">
-            <input hidden name="showEpisodeId" v-model="form.showEpisodeId">
+        <input name="movieId" v-model="form.movieId">
         </form>
 
     </div>
@@ -20,27 +17,22 @@
 import { Dropzone } from "dropzone";
 import { useForm } from "@inertiajs/inertia-vue3"
 import { useUserStore } from "@/Stores/UserStore";
-import { useVideoPlayerStore } from "@/Stores/VideoPlayerStore";
 import { onMounted, ref } from "vue";
 import { Inertia } from "@inertiajs/inertia";
 
 let userStore = useUserStore()
-let videoPlayerStore = useVideoPlayerStore()
-
 let uploadPercentage = ref(0);
 let uploadingMessage = ref(0);
 let uploadCompleteMessage = ref(0);
-// let isHidden = ref(false);
+let isHidden = ref(false);
 
 onMounted(() => {
-    videoPlayerStore.videoUploadComplete = false;
 // see options for Dropzone here: https://github.com/dropzone/dropzone/blob/main/src/options.js
 let myDropzone = new Dropzone("#videoUploadForm", {
     url: "/videoupload",
     paramName: "file", // The name that will be used to transfer the file
     maxFilesize: '50 GB', // MB
     chunking: true,
-    timeout: "2100",
     chunkSize: 2 * 1024 * 1024,
     parallelChunkUploads: false,
     retryChunks: false,
@@ -53,9 +45,9 @@ let myDropzone = new Dropzone("#videoUploadForm", {
     uploadprogress: function(file, progress, bytesSent) {
         userStore.uploadPercentage = progress;
         console.log(userStore.uploadPercentage);
-        // if(userStore.uploadPercentage !== 100){
-        //     // isHidden = true;
-        // }
+        if(userStore.uploadPercentage !== 100){
+            isHidden = true;
+        }
     },
     dictDefaultMessage: "Click here or Drop files here to upload",
     forceFallback: false, // for testing, set to true.
@@ -78,12 +70,10 @@ myDropzone.on("complete", function(file) {
     uploadCompleteMessage = 1;
     myDropzone.removeFile(file);
     userStore.uploadPercentage = 0;
-    // isHidden = false;
+    isHidden = true;
     Inertia.reload({
         only: ["videos"],
     });
-    videoPlayerStore.videoUploadComplete = true;
-
 });
 
 })
@@ -110,8 +100,8 @@ let form = useForm({
     file: [],
     // movieId: userStore.uploadMovieId,
     movieId: props.movieId,
-    movieTrailerId: props.movieTrailerId,
-    showEpisodeId: props.showEpisodeId,
+    movieTrailerId: props.movieId,
+    showEpisodeId: props.uploadShowEpisodeId,
 });
 
 // let props = defineProps({
@@ -127,17 +117,6 @@ let form = useForm({
 </script>
 <style scoped>
 
-/*Original Dropzone Styling*/
-/*.dropzone {*/
-/*    display: flex;*/
-/*    flex-direction: column;*/
-/*    justify-content: center;*/
-/*    align-items: center;*/
-/*    row-gap: 16px;*/
-/*    border: 2px dashed #000000;*/
-/*    transition: 0.3s ease all;*/
-/*}*/
-
 .dropzone {
     display: flex;
     flex-direction: column;
@@ -145,7 +124,6 @@ let form = useForm({
     align-items: center;
     row-gap: 16px;
     border: 2px dashed #000000;
-    background-color: #fce4bb;
     transition: 0.3s ease all;
 }
 
