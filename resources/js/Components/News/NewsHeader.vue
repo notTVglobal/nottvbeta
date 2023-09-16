9ae312192352573f50d7cb2131bf5c367a5eef9b
Travis Cross (Developer) <developer@MacBook-Pro.local>
2023-09-16 02:58:12 -0700

    remove bottom border from newHeader

--- resources/js/Components/News/NewsHeader.vue
+++ resources/js/Components/News/NewsHeader.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="container mx-auto flex flex-col lg:flex-row items-center justify-start px-4 py-6 mb-3 border-b border-gray-500">
+    <div class="container mx-auto flex flex-col lg:flex-row items-center justify-start px-4 py-6 mb-3">
 <!--    <div class="flex flex-col lg:flex-row items-center">-->
         <h1 class="text-3xl font-semibold text-center lg:text-left"><slot /></h1>
         <ul class="flex ml-0 lg:ml-16 mt-6 mr-6 lg:mt-0 space-x-8" >
