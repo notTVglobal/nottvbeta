10adc19f766fe824afeb56a0ab572c2bbe129036
Travis Cross <travis@not.tv>
2024-02-07 00:49:49 -0800

    add 503 maintenance mode page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maintenance Mode</title>
+    <style>
+        body { text-align: center; padding: 150px; }
+        h1 { font-size: 50px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+        a { color: #dc8100; text-decoration: none; }
+        a:hover { color: #333; text-decoration: none; }
+    </style>
+</head>
+<body>
+<article>
+    <img src="https://not.tv/storage/images/logo_black_311.png" class="w-full" alt="notTV Logo">
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:hello@not.tv">contact us</a>, otherwise, we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The notTV Team</p>
+    </div>
+</article>
+</body>
+</html>
