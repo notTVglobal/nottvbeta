879858e53ee8fcee8574fb15832553f13fa009b2
Travis Cross <travis@not.tv>
2024-02-13 01:44:28 -0800

    small tweaks to css on small screens show index page

--- resources/js/Components/Global/Badges/CreatorsOnlyBadge.vue
+++ resources/js/Components/Global/Badges/CreatorsOnlyBadge.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div class="badge bg-accent border-accent text-accent-content drop-shadow-lg">Creators Only</div>
+  <div class="badge bg-accent border-accent text-accent-content drop-shadow-lg badge-xs md:badge-md py-2 md:py-0">Creators Only</div>
 </div>
 </template>
 <script setup>
--- resources/js/Components/Global/Badges/NewContentBadge.vue
+++ resources/js/Components/Global/Badges/NewContentBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="badge bg-yellow-500 border-yellow-500 text-black drop-shadow-lg">New</div>
+    <div class="badge bg-yellow-500 border-yellow-500 text-black drop-shadow-lg badge-xs md:badge-md py-2 md:py-0">New</div>
   </div>
 </template>
 <script setup>
