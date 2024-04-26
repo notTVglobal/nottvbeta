7c27598292be113c03ce61a1de587c54e0404d3a
Travis Cross <travis@not.tv>
2024-04-26 16:56:09 -0700

    block another bot

--- app/Http/Middleware/PreventSessionByUserAgent.php
+++ app/Http/Middleware/PreventSessionByUserAgent.php
@@ -15,7 +15,7 @@ class PreventSessionByUserAgent
  */
     public function handle(Request $request, Closure $next) {
       // List of user agents to not start sessions for
-      $blockedUserAgents = ['DigitalOcean Uptime Probe 0.22.0 (https://digitalocean.com)'];
+      $blockedUserAgents = ['DigitalOcean Uptime Probe 0.22.0 (https://digitalocean.com)', 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; ClaudeBot/1.0; +claudebot@anthropic.com)'];
 
       // Check if the current user agent is in the blocked list
       if (in_array($request->header('User-Agent'), $blockedUserAgents)) {
