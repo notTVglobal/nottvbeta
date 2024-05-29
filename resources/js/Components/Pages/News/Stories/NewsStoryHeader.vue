4dd68392c373fa209c49081d206a200cfddccc96
Travis Michael Cross <travis@not.tv>
2024-05-29 07:07:32 -0700

    fix erroneous applySearch function in the NewsController

--- resources/js/Components/Pages/News/Stories/NewsStoryHeader.vue
+++ resources/js/Components/Pages/News/Stories/NewsStoryHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="mb-4">
     <header class="place-self-center flex flex-col w-full text-black bg-gray-800">
       <PublicNewsNavigationButtons class=""/>
     </header>
 
-    <div class="w-full max-w-7xl mx-auto ">
+    <div class="w-full max-w-7xl mx-auto">
       <Breadcrumbs :classType="'dark'"
                    :breadcrumbs="[{ text: 'News Stories', to: '/news' }, { text: $page.props.newsStory.category.name, to: `/news/category/${$page.props.newsStory.category.slug}` }, { text: $page.props.newsStory.subCategory.name, to: '' }]"/>
     </div>
